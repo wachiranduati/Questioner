5e408bcda198c7c4eddb28d461fb6fbd8af58fda
wachiranduati <wachiranduati@gmail.com>
2019-01-08 21:25:07 +0300

    Improve the test code just enough to pass

--- app/tests/v1/test_adminuser.py
+++ app/tests/v1/test_adminuser.py
@@ -1,5 +1,6 @@
 import unittest
 from app import create_app
+from flask import jsonify, json
 
 
 class TestAdminEndpoints(unittest.TestCase):
@@ -9,7 +10,19 @@ class TestAdminEndpoints(unittest.TestCase):
 		self.client = self.app.test_client()
 
 	def test_createmeetup(self):
-		assertEqual(4, 0)
+		self.response_message = self.client.post('api/v1/meetups',
+			data=json.dumps(
+				{
+"id" : 2,
+"createdOn" : "12-23-2019",
+"location" : "Kangundo",
+"images" : ["https://placeholder.io", "https://placeingine.io"],
+"topic" : "Biking to MT longonot",
+"happeningOn" : "12-23-2019",
+"Tags" : ["bike", "bonding", "cheki maneno"]
+}
+), content_type="application/json")
+		self.assertEqual(self.response_message.status_code, 200)
 
 	def tearDown(self):
 		self.app = None
