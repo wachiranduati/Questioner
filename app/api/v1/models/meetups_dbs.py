f905547d0d2513668d42d432905b2de72456c43f
wachiranduati <wachiranduati@gmail.com>
2019-01-08 21:14:00 +0300

    [Start #163032301] Create meetups database

---
+++ app/api/v1/models/meetups_dbs.py
@@ -0,0 +1,39 @@
+meetups = [
+{
+"id" : 1,
+"createdOn" : "12-27-2019",
+"location" : "Kanjahi",
+"images" : ["https://farmlands.io", "https://unsplash.com/21"],
+"topic" : "Swimming up the gulf sea",
+"happeningOn" : "2-03-2013",
+"Tags" : ["bike", "bonding", "cheki maneno"]
+},
+{
+"id" : 2,
+"createdOn" : "12-03-2009",
+"location" : "Kangundo",
+"images" : ["https://graceland.io", "https://whatelse.io"],
+"topic" : "Freedom scavenging",
+"happeningOn" : "10-12-2010",
+"Tags" : ["bike", "bonding", "cheki maneno"]
+}
+,
+{
+"id" : 3,
+"createdOn" : "11-20-2010",
+"location" : "Kangundo",
+"images" : ["https://mumathai.io", "https://thegoings.io"],
+"topic" : "The only grace",
+"happeningOn" : "01-23-2012",
+"Tags" : ["bike", "bonding", "cheki maneno"]
+}
+,{
+"id" : 4,
+"createdOn" : "15-23-2017",
+"location" : "Kangundo",
+"images" : ["https://mukindorio.io", "https://imeweseko.io"],
+"topic" : "Coffe and lunch",
+"happeningOn" : "02-21-2018",
+"Tags" : ["bike", "bonding", "cheki maneno"]
+}
+]
