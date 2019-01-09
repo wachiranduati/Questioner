c843447e1fe985de83d120cdec1cbeeceba79e6a
wachiranduati <wachiranduati@gmail.com>
2019-01-09 08:36:44 +0300

    [Finishes #163033078] Create more tests and tweak the api code to handle more cases

--- app/api/v1/views/user_views.py
+++ app/api/v1/views/user_views.py
@@ -22,5 +22,24 @@ class getspecific(Resource):
 		for meet_up in meetups:
 			if meet_up['id'] == id:
 				return {"status":200,
-					 "data": [meet_up]
+					 "data": [
+					 {
+					 "id":meet_up['id'],
+					 "topic":meet_up['topic'],
+					 "location":meet_up['location'],
+					 "happeningOn":meet_up['happeningOn'],
+					 "tags": meet_up['Tags']
+					 }
+					 ]
 					 }, 200
+
+@UserApi.route('/api/v1/meetups/<string:id>')
+@UserApi.route('/api/v1/meetups/<float:id>')
+@UserApi.route('/api/v1/meetups/<path:id>')
+@UserApi.route('/api/v1/meetups/<uuid:id>')
+class getspecificerrors(Resource):
+	def get(self,id):
+		return {
+				"status" : 400,
+				"error" : "Sorry this route only supports Integers as part of its variable rules"
+				}, 400
--- app/tests/v1/test_user.py
+++ app/tests/v1/test_user.py
@@ -11,9 +11,19 @@ class TestUserEndpoints(unittest.TestCase):
 		self.response_message = self.client.get('/api/v1/meetups/upcoming')
 		self.assertEqual(self.response_message.status_code, 200)
 
-	def test_getspecificmeetup(self):
-		self.response_message = self.client.get('/api/v1/meetups/upcoming')
+	def test_getspecificmeetup_success(self):
+		self.response_message = self.client.get('/api/v1/meetups/1')
 		self.assertEqual(self.response_message.status_code, 200)
+
+	def test_getspecificmeetup_different_data_type(self):
+		self.response_message = self.client.get('/api/v1/meetups/girl')
+		self.assertEqual(self.response_message.status_code, 400)
+		self.response_message = self.client.get('/api/v1/meetups/2.3')
+		self.assertEqual(self.response_message.status_code, 400)
+		self.response_message = self.client.get('/api/v1/meetups/girl/boy')
+		self.assertEqual(self.response_message.status_code, 400)
+
+
 
 
 	def tearDown(self):
