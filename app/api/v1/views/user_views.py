743998d91848a5e8b2fbeb67a1408ae09c2870da
wachiranduati <wachiranduati@gmail.com>
2019-01-09 06:49:08 +0300

    [Start #163033078] Update blueprint name to a more relevant name

--- app/api/v1/views/user_views.py
+++ app/api/v1/views/user_views.py
@@ -5,11 +5,11 @@ from app.api.v1.models.meetups_dbs import meetups
 from datetime import datetime
 
 
-users = Blueprint('apius', __name__)
-apius = Api(users)
+users = Blueprint('UserApi', __name__)
+UserApi = Api(users)
 
 
-@apius.route('/api/v1/meetups/upcoming')
+@UserApi.route('/api/v1/meetups/upcoming')
 class getall(Resource):
 	def get(self):
 		return {"status":200,
