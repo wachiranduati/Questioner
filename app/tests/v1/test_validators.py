a95da5b2509377bb6ec74f753411c3fbd746be7f
wachiranduati <wachiranduati@gmail.com>
2019-01-12 03:01:50 +0300

    [Finishes #163163503] create failing test

--- app/tests/v1/test_validators.py
+++ app/tests/v1/test_validators.py
@@ -4,4 +4,13 @@ from app.api.v1.utils.validator import TestValidator
 
 
 class ValidatorTestSys(unittest.TestCase):
-	
+	def setUp(self):
+		self.app = create_app("testing")
+		self.client = self.app.test_client()
+		validt = TestValidator()
+
+	def test_empty(self):
+		pass
+
+	def tearDown(self):
+		self.app = None
